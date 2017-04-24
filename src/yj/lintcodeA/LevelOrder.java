3e505f9a5e5525e66683fb98526e3c279cec3e2d
XiaoYueJian <yjbug@qq.com>
2017-04-24 17:26:27 +0800

    printLeft

--- src/yj/lintcodeA/LevelOrder.java
+++ src/yj/lintcodeA/LevelOrder.java
@@ -22,4 +22,46 @@ public class LevelOrder {
 			}
 		}
 	}
+	
+	// 从左往右看二叉树，只显示看到的节点
+	public void printLeft(TreeNode root){
+		if(root==null){
+			return;
+		}
+		Queue<TreeNode> q = new LinkedList<>();
+		// initialize
+		q.add(root);
+		int curSize = 1;
+		int nextSize = 0;
+		boolean first = true;
+		while(q.size()!=0){
+			if(first){
+				first = false;
+				System.out.println(q.peek().val);
+			}
+			TreeNode cur =  q.poll();
+			curSize--;
+			if(cur.left!=null){
+				q.add(cur.left);
+				nextSize++;
+			}
+			if(cur.right!=null){
+				q.add(cur.right);
+				nextSize++;
+			}
+			//  判断当前层是否结束
+			if(curSize==0){
+				first = true;
+				curSize = nextSize;
+				nextSize = 0;
+			}	
+		}
+	}
+	
+	public static void main(String[] args) {
+		LevelOrder T = new LevelOrder();
+		T.printLeft(TreeNode.getTree());
+	}
+	
+
 }
